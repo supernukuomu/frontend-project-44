2303d460b8522060c21a6fb5b1003f7979a4b3db
supernukuomu <kpa6ac@gmail.com>
2024-04-20 12:45:18 +0300

    modified progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,21 +1,28 @@
 import getGameInfo from "../index.js";
 import getRandomInt from "../random.js";
 
+const getArray = () => {
+  let arr = [];
+  let nextNum = getRandomInt(0, 30);
+  const PROGRESSIONLENGTH = 10;
+  const progressionStep = getRandomInt(2, 5);
+
+  for (let i = 0; i < PROGRESSIONLENGTH; i++) {
+    nextNum += progressionStep;
+    arr.push(nextNum);
+  }
+  return arr;
+};
 
 const getCorrectAnswer = () => {
-  const PROGRESSIONLENGTH = 10;
-  const resultArr = [];
-  resultArr[0] = getRandomInt(0, 30);
-  const progressionStep = getRandomInt(0. 5);
-  for (let i = 1; i < PROGRESSIONLENGTH; i ++) {
-    resultArr[i] += progressionStep;
+  const arrayOfNumbers = getArray();
+  const hiddenNumberPosition = getRandomInt(2, 9);
 
-
-  const question = `${firstRandomNumber} ${secondRandomNumber}`;
-  const correctAnswer = getNod(
-    firstRandomNumber,
-    secondRandomNumber
-  ).toString();
+  let hiddenNumber = arrayOfNumbers[hiddenNumberPosition];
+  const correctAnswer = hiddenNumber.toString();
+  arrayOfNumbers[hiddenNumberPosition] = "..";
+  let resultingArrayOfNumbers = arrayOfNumbers.join(" ");
+  const question = `${resultingArrayOfNumbers}`;
   return [question, correctAnswer];
 };
 
