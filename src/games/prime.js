e603ce73b69c108b527a72dae21d48c7f4d46e21
supernukuomu <kpa6ac@gmail.com>
2024-04-20 21:39:33 +0300

    added prime to "games"

---
+++ src/games/prime.js
@@ -0,0 +1,19 @@
+import getGameInfo from "../index.js";
+import getRandomInt from "../random.js";
+
+const isSimple = (n, a = 2) => a ** n % n === a;
+
+const getCorrectAnswer = () => {
+  const num = getRandomInt(3, 30);
+  const correctAnswer = isSimple(num) ? "yes" : "no";
+  const question = `${num}`;
+  return [question, correctAnswer];
+};
+
+const startBrainPrime = () => {
+  const thatIsTheQuestion =
+    'Answer "yes" if given number is prime. Otherwise answer "no".';
+  getGameInfo(getCorrectAnswer, thatIsTheQuestion);
+};
+
+export default startBrainPrime;
