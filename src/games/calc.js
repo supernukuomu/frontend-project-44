9622a868e4caad650f0e16cb172efb2ac58f395d
supernukuomu <kpa6ac@gmail.com>
2024-04-22 20:55:21 +0300

    fixed calc.js

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,36 +1,35 @@
-import getGameInfo from "../index.js";
-import getRandomInt from "../random.js";
+import getGameInfo from '../index.js';
+import getRandomInt from '../random.js';
 
 const calculate = (a, b, operator) => {
   switch (operator) {
-    case "+":
+    case '+':
       return a + b;
-    case "-":
+    case '-':
       return a - b;
-    case "*":
+    case '*':
       return a * b;
     default:
-      return "null";
+      return 'null';
   }
 };
 
-const getCorrectAnswer = () => {
+const getGameData = () => {
   const firstRandomNumber = getRandomInt(0, 30);
   const secondRandomNumber = getRandomInt(0, 30);
-  const operators = ["+", "-", "*"];
+  const operators = ['+', '-', '*'];
   const randomOperator = operators[getRandomInt(0, operators.length - 1)];
   const question = `${firstRandomNumber} ${randomOperator} ${secondRandomNumber}`;
   const correctAnswer = calculate(
     firstRandomNumber,
     secondRandomNumber,
-    randomOperator
-  ).toString();
+    randomOperator).toString();
   return [question, correctAnswer];
 };
 
 const startBrainCalc = () => {
-  const thatIsTheQuestion = "What is the result of the expression?";
-  getGameInfo(getCorrectAnswer, thatIsTheQuestion);
+  const thatIsTheQuestion = 'What is the result of the expression?';
+  getGameInfo(getGameData, thatIsTheQuestion);
 };
 
 export default startBrainCalc;
