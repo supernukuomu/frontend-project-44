4525bb7641103410399cfdf7efb4943816db415f
supernukuomu <kpa6ac@gmail.com>
2024-04-22 21:28:19 +0300

    fixed

--- src/index.js
+++ src/index.js
@@ -1,25 +1,23 @@
-import readlineSync from "readline-sync";
+import readlineSync from 'readline-sync';
 
 const getGameInfo = (gameData, thatIsTheQuestion) => {
-  console.log("Welcome to the Brain Games!");
-  const userName = readlineSync.question("May I have your name? ");
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
 
   console.log(thatIsTheQuestion);
 
   const ATTEMPTS = 3;
-  for (let i = 1; i <= ATTEMPTS; i++) {
+  for (let i = 0; i < ATTEMPTS; i += 1) {
     const [question, correctAnswer] = gameData();
     console.log(`Question: ${question}`);
-    const userAnswer = readlineSync.question("Your answer: ");
+    const userAnswer = readlineSync.question('Your answer: ');
     if (correctAnswer !== userAnswer) {
-      console.log(
-        `'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`
-      );
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
       console.log(`Let's try again, ${userName}!`);
       return;
     }
-    console.log("Correct!");
+    console.log('Correct!');
   }
   console.log(`Congratulations, ${userName}!`);
 };
