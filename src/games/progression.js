65b03a95ae512ecf23ef70c2815dce040b9525f2
supernukuomu <kpa6ac@gmail.com>
2024-04-22 21:27:48 +0300

    fixed

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,34 +1,34 @@
-import getGameInfo from "../index.js";
-import getRandomInt from "../random.js";
+import getGameInfo from '../index.js';
+import getRandomInt from '../random.js';
 
-const getArray = () => {
-  let arr = [];
-  let nextNum = getRandomInt(0, 30);
+const getRandomNumArray = () => {
+  const arr = [];
+  const firstNum = getRandomInt(0, 30);
   const PROGRESSIONLENGTH = 10;
   const progressionStep = getRandomInt(2, 5);
 
-  for (let i = 0; i < PROGRESSIONLENGTH; i++) {
-    nextNum += progressionStep;
+  for (let i = 0; i < PROGRESSIONLENGTH; i += 1) {
+    const nextNum = firstNum + progressionStep * i;
     arr.push(nextNum);
   }
   return arr;
 };
 
-const getCorrectAnswer = () => {
-  const arrayOfNumbers = getArray();
+const getGameData = () => {
+  const arrayOfNumbers = getRandomNumArray();
   const hiddenNumberPosition = getRandomInt(2, 9);
 
-  let hiddenNumber = arrayOfNumbers[hiddenNumberPosition];
+  const hiddenNumber = arrayOfNumbers[hiddenNumberPosition];
   const correctAnswer = hiddenNumber.toString();
-  arrayOfNumbers[hiddenNumberPosition] = "..";
-  let resultingArrayOfNumbers = arrayOfNumbers.join(" ");
+  arrayOfNumbers[hiddenNumberPosition] = '..';
+  const resultingArrayOfNumbers = arrayOfNumbers.join(' ');
   const question = `${resultingArrayOfNumbers}`;
   return [question, correctAnswer];
 };
 
 const startBrainProgression = () => {
-  const thatIsTheQuestion = "What number is missing in the progression?";
-  getGameInfo(getCorrectAnswer, thatIsTheQuestion);
+  const thatIsTheQuestion = 'What number is missing in the progression?';
+  getGameInfo(getGameData, thatIsTheQuestion);
 };
 
 export default startBrainProgression;
