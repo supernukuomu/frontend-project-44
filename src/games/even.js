ad50e411c3cca365f8c6e550759b50b6843453b6
supernukuomu <kpa6ac@gmail.com>
2024-04-22 21:26:31 +0300

    fixed

--- src/games/even.js
+++ src/games/even.js
@@ -1,18 +1,17 @@
-import getGameInfo from "../index.js";
-import getRandomInt from "../random.js";
+import getGameInfo from '../index.js';
+import getRandomInt from '../random.js';
 
 const isEven = (number) => number % 2 === 0;
 
-const getCorrectAnswer = () => {
+const getGameData = () => {
   const num = getRandomInt(0, 100);
-  const correctAnswer = isEven(num) ? "yes" : "no";
+  const correctAnswer = isEven(num) ? 'yes' : 'no';
   return [num, correctAnswer];
 };
 
 const startBrainEven = () => {
-  const thatIsTheQuestion =
-    'Answer "yes" if the number is even, otherwise answer "no".';
-  getGameInfo(getCorrectAnswer, thatIsTheQuestion);
+  const thatIsTheQuestion = 'Answer "yes" if the number is even, otherwise answer "no".';
+  getGameInfo(getGameData, thatIsTheQuestion);
 };
 
 export default startBrainEven;
