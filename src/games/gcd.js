6ebce7159176340a418c2ce66017da27452163a0
supernukuomu <kpa6ac@gmail.com>
2024-04-22 21:26:47 +0300

    fixed

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,23 +1,19 @@
-import getGameInfo from "../index.js";
-import getRandomInt from "../random.js";
+import getGameInfo from '../index.js';
+import getRandomInt from '../random.js';
 
 const getNod = (a, b) => (b !== 0 ? getNod(b, a % b) : a);
 
-const getCorrectAnswer = () => {
+const getGameData = () => {
   const firstRandomNumber = getRandomInt(0, 30);
   const secondRandomNumber = getRandomInt(0, 30);
   const question = `${firstRandomNumber} ${secondRandomNumber}`;
-  const correctAnswer = getNod(
-    firstRandomNumber,
-    secondRandomNumber
-  ).toString();
+  const correctAnswer = getNod(firstRandomNumber, secondRandomNumber).toString();
   return [question, correctAnswer];
 };
 
 const startBrainGcd = () => {
-  const thatIsTheQuestion =
-    "Find the greatest common divisor of given numbers.";
-  getGameInfo(getCorrectAnswer, thatIsTheQuestion);
+  const thatIsTheQuestion = 'Find the greatest common divisor of given numbers.';
+  getGameInfo(getGameData, thatIsTheQuestion);
 };
 
 export default startBrainGcd;
